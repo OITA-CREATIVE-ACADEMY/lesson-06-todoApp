8ce26676f6b4b2420a13264c80c6d6211870a662
tsk <tsk.wakabayashi@gmail.com>
2018-10-11 18:04:16 +0900

    タスクの変更 実装

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,12 @@
 $(function(){
-    var todoRef = firebase.database().ref('/');
 
+    var ROOT_PATH = '/tasks/';
+
+    var todoRef = firebase.database().ref(ROOT_PATH);
+
+    /**
+     * タスクの追加
+     */
     $('.add_btn').on('click',function() {
         // タスク名取得
         var addTask = $('#add_task').val()
@@ -14,12 +20,47 @@ $(function(){
         });
         $('#add_task').val('');
     });
+
+    /**
+     * タスクの参照
+     */
     todoRef.on('child_added', function (snapshot) {
         var taskData = snapshot.val();
         if (taskData.task) {
             var cloneTask = $('#task-dummy').find('div.panel').clone(true);
             cloneTask.find('.task_name').text(taskData.task);
+            cloneTask.find('.task_name').attr('data-key',snapshot.key);
             $('#task-list').append(cloneTask);
         }
     });
+
+    /**
+     * タスクの変更
+     */
+    $('.edit_btn').on('click',function(e) {
+        var $target = $(e.target);
+
+        // タスク名取得
+        var taskData = $($target).parents('div.task-body').find('span.task_name');
+        var taskName = taskData.text();
+        var taskId = taskData.data('key');
+        $('#edit_task').val(taskName);
+        $('#task-id').val(taskId);
+        $('#edit_task').focus();
+    });
+
+    $('.update_done_btn').on('click',function(e) {
+
+        var taskName = $('#edit_task').val();
+        var taskId = $('#task-id').val();
+
+        var updates = {};
+        updates[taskId + '/task'] = taskName;
+        if (todoRef.update(updates)) {
+            $('[data-key=' + taskId + ']').text(taskName);
+        }
+      
+        return ;
+    });
+
 });

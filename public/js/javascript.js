ae1cce5b3c04288264363c1983dfbb42dd1aa9ba
KazukiMatsuda <madakazuki@ezweb.ne.jp>
2018-10-29 09:44:16 +0900

    [add]style.cssの追加
    タスクの表示を大きく、追加ボタンを丸くした。

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,20 +1,18 @@
 var database = firebase.database().ref('/'); //データベースでデータの読み書きを行うインスタンス,refはルート
+var taskKey;//タスクのkey
 $(function(){
    // モーダルを出す
    $('.modal_btn').on('click',function(e) {
-     console.log(e.target);
     if ($(e.target).hasClass('add_modal')) {
-      //追加のモーダル
+      //追加用のモーダル
       $('.modal-add').show();
       $('.modal-edit').hide();
       $('.modal-title').html('タスクを追加する');
     } else {
-      // 編集のモーダル
+      // 編集用のモーダル
       $('.modal-edit').show();
       $('.modal-add').hide();
-      // タスク名取得  
     }
-
   });
   /*表示*/
   database.on('value', function (snapshot) { //イベントハンドラ、dataabaseに接続している
@@ -24,65 +22,50 @@ $(function(){
     var childData = childSnapshot.val();
     console.log(childData.name);
     $('#todoDiv').prepend(
-      '<div class="todolist">'
+      '<div class="todolist panel panel-primary panel-body task-name">'
       +childData.name+
-      '<input type="button" class="btn btn-danger" value="削除" id='
+      '<div class="pull-right"><input type="button" class="btn btn-danger" value="削除" id='
       +childKey+
       ' onclick="del(this);"/> <input type="button" class="btn btn-warning" value="編集" id='
       +childKey+
-      ' data-toggle="modal" data-target="#sampleModal" onclick="edit(this);" /></div>');   
+      ' data-toggle="modal" data-target="#sampleModal" onclick="edit(this);" /></div></div>');   
     });
   });
 });
-/*追加*/
+/*追加ボタン*/
 function add(){
-  console.log("追加できてるよ");
   var name = $('#todo-add').val();
   database.push({name:name});
 };
-/*削除*/
+/*削除ボタン*/
 function del(ele){
   database.child(ele.id).remove();
 };
+/*モーダル内の削除ボタン*/
 function del2(){
-  const taskKey= $('#comeId').text();
   database.child(taskKey).remove();
 }
+/*編集ボタン*/
 function edit(ele){
- console.log("edit押せてるよ");
- console.log(ele);
- const item = $(ele).parent('.todolist').text();
- console.log(item);
-// 編集中のタスクのIDを取得  
-const taskKey= $(ele).attr('id');
-console.log(taskKey);
-
-
-$('#comeId').html(taskKey);
-$('#todo-edit').val(item);//モーダルに値を渡している
-
-
- $('.modal-edit').show();     
- $('.modal-add').hide();
- $('.modal-title').html('タスクを編集する');//html()前のは消される
-
+  const item = $(ele).parent('.todolist').text();
+  // 編集中のタスクのIDを取得  
+  taskKey= $(ele).attr('id');
+  $('#todo-edit').val(item);//モーダルに値を渡している
+  $('.modal-edit').show();     
+  $('.modal-add').hide();
+  $('.modal-title').html('タスクを編集する');//html()前のは消される
 };
 /*空文字を入れる*/
-function  emp(){
+function emp(){
   $('#todo-add').val('');
 };
+/*完了ボタン*/
 function updata(){
   // タスクの入力欄の値を取得
  const newTask = $('#todo-edit').val();
- console.log(newTask);
-// 編集中のタスクのIDを取得  
- const comeKey = $('#comeId').text();
- console.log(comeKey);
-// json形式でデータを作成
-var updateData = {};
-updateData[comeKey] = {'name': newTask};
-console.log(updateData);
-// 変更内容を保存
-var success = database.update(updateData);
-console.log(success);//     $(span_task).html(editTask);
+  // json形式でデータを作成
+  var updateData = {};
+  updateData[taskKey] = {'name': newTask};
+  // 変更内容を保存
+  var success = database.update(updateData);
 };
